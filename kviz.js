2a7d9a8486fa63d075679a403c6ff974061462b9
jeskyncino <mail@helakonecna.cz>
2021-04-29 00:48:06 +0200

    done

--- kviz.js
+++ kviz.js
@@ -18,6 +18,12 @@ let otazky = [
         odpovedi: ["Umět JavaScript", "Chodit po kurzu do hospody"],
         indexSpravne: 0
     },
+    {
+        otazka: "Kterého kouče tímto zdravím? :)",
+        foto: "obrazky/czechitas.png",
+        odpovedi: ["Filipa", "Hanču", "Verču", "Radomilu", "Moniku"],
+        indexSpravne: 1
+    }
 ];
 
 const odpovedi = document.querySelector("#odpovedi");
@@ -40,47 +46,53 @@ function vypisOtazku() {
         polozka.dataset.odpoved = i;
         polozka.innerHTML = otazky[indexOtazky].odpovedi[i];
         odpovedi.appendChild(polozka);
-
-        polozka.addEventListener("click", function() {
-            zvoleneOdpovedi.push(this.dataset.odpoved);
-            odpovedi.innerHTML = "";
-            if (indexOtazky < pocetOtazek) {
-                vypisOtazku()
-            } else {
-                document.querySelector(".kviz").style.display = "none";
-                let vysledky = document.querySelector(".vysledek")
-                vysledky.style.display = "block";
-
-                for (i = 0; i < pocetOtazek; i++) {
-                    let polozka = document.createElement("div");
-                    polozka.classList.add("vysledekOtazky");
-                    let otazkaPolozky = document.createElement("h3")
-                    otazkaPolozky.innerHTML = (i + 1) + ". " + otazky[i].otazka;
-                    let zvolenaOdpoved = document.createElement("p");
-                    zvolenaOdpoved.innerHTML = "Tvoje odpověď: " + otazky[i].odpovedi[zvoleneOdpovedi[i]]
-                    let spravnaOdpoved = document.createElement("p");
-                    if (zvoleneOdpovedi[i] == otazky[i].indexSpravne) {
-                        spravnaOdpoved.innerHTML = "Toto je SPRÁVNĚ.";
-                        pocetSpravnych += 1;
-                    } else {
-                        spravnaOdpoved.innerHTML = "Správná odpověď: " + otazky[i].odpovedi[otazky[i].indexSpravne];
-                    }
-                    vysledky.appendChild(polozka);
-                    polozka.appendChild(otazkaPolozky);
-                    polozka.appendChild(zvolenaOdpoved);
-                    polozka.appendChild(spravnaOdpoved);
-                }
-
-                let uspesnost = document.createElement("h2");
-                let procentoUspechu = ((pocetSpravnych * 100) / pocetOtazek);
-                uspesnost.innerHTML = "Správně " + pocetSpravnych + " ze " + pocetOtazek + " otázek. Úspěšnost " + Math.floor(procentoUspechu) + "%."
-                vysledky.appendChild(uspesnost);
-            }
-        })
+        polozka.addEventListener("click", priKliknuti)
     }
 
     indexOtazky += 1;
+
 }
 
+function priKliknuti() {
+    zvoleneOdpovedi.push(this.dataset.odpoved);
+    odpovedi.innerHTML = "";
+    if (indexOtazky < pocetOtazek) {
+        vypisOtazku()
+    } else {
+        vypisVysledky()
+    }
+}
+
+
+function vypisVysledky() {
+    document.querySelector(".kviz").style.display = "none";
+    let vysledky = document.querySelector(".vysledek")
+    vysledky.style.display = "block";
+
+    for (i = 0; i < pocetOtazek; i++) {
+        let polozka = document.createElement("div");
+        let otazkaPolozky = document.createElement("h3")
+        otazkaPolozky.innerHTML = (i + 1) + ". " + otazky[i].otazka;
+        let zvolenaOdpoved = document.createElement("p");
+        zvolenaOdpoved.innerHTML = "Tvoje odpověď: " + otazky[i].odpovedi[zvoleneOdpovedi[i]]
+        let spravnaOdpoved = document.createElement("p");
+        if (zvoleneOdpovedi[i] == otazky[i].indexSpravne) {
+            spravnaOdpoved.innerHTML = "Toto je SPRÁVNĚ.";
+            pocetSpravnych += 1;
+        } else {
+            spravnaOdpoved.innerHTML = "Správná odpověď: " + otazky[i].odpovedi[otazky[i].indexSpravne];
+        }
+        vysledky.appendChild(polozka);
+        polozka.appendChild(otazkaPolozky);
+        polozka.appendChild(zvolenaOdpoved);
+        polozka.appendChild(spravnaOdpoved);
+    }
+
+    let uspesnost = document.createElement("h2");
+    let procentoUspechu = ((pocetSpravnych * 100) / pocetOtazek);
+    uspesnost.innerHTML = "Správně " + pocetSpravnych + " ze " + pocetOtazek + " otázek. Úspěšnost " + Math.floor(procentoUspechu) + "%."
+    vysledky.appendChild(uspesnost);
+
+}
 
 vypisOtazku()
